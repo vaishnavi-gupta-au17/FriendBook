45236c9137f8a63b5aa869ded76e9c8e8937639f
Vaishnavi Gupta <vaishnaviguptamakeup@gmail.com>
2022-01-29 14:02:48 +0530

    social

---
+++ FriendBook Project/frontend/src/App.js
@@ -0,0 +1,9 @@
+import Home from "./pages/home/Home";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Person } from "@material-ui/icons";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
